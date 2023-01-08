68e4e8dd78ff29e53fc7b8d8d52e3bcf7f0b1c36
Justin Pulley <justinpulley92@gmail.com>
2023-01-08 17:13:24 -0600

    Refactored to use Euler for flashloans, zero fee and only requires swapping the received collateral.

--- script/Liqqer.s.sol
+++ script/Liqqer.s.sol
@@ -5,9 +5,6 @@ import "forge-std/Script.sol";
 import "../src/Liqqer.sol";
 
 contract MyScript is Script {
-    IWETH private immutable WETH =
-        IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-
     address me = address(0x00000000000E898D039D41D5aB37738F3aeD833D);
 
     // Just deploys at sets owner to our efficient caller address
--- src/Liqqer.sol
+++ src/Liqqer.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "./utils/Margin.sol";
+import "./utils/IEuler.sol";
 import "solmate/auth/Owned.sol";
 import "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
 
@@ -16,11 +17,6 @@ contract Liqqer is Owned {
     using SafeERC20 for IERC20;
 
     uint256 MAX_INT = 2**256 - 1;
-    ISoloMargin private immutable soloMargin =
-        ISoloMargin(0x1E0447b19BB6EcFdAe1e4AE1694b0C3659614e4e);
-
-    IWETH private immutable WETH =
-        IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
 
     ILendingPool private immutable AAVE =
         ILendingPool(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9);
@@ -28,124 +24,47 @@ contract Liqqer is Owned {
     address private immutable mySafe =
         0x3224C6C9BCA033CAbB85A36891EBf994732d8f23;
 
-    constructor(address _owner) Owned(_owner) {
-        WETH.approve(address(soloMargin), MAX_INT);
+    constructor(address _owner) Owned(_owner) {}
+
+    function flash_WaC(ILiqqer.CallParams calldata params) external onlyOwner {
+        IEulerDToken dToken = IEulerDToken(
+            EulerAddrsMainnet.markets.underlyingToDToken(params.debtA)
+        );
+        dToken.flashLoan(params.debtToCover, abi.encode(params));
     }
 
-    function flashLoan_UNy(
-        uint256 loanAmount,
-        ILiqqer.CallParams calldata _params
-    ) external onlyOwner {
-        Actions.ActionArgs[] memory operations = new Actions.ActionArgs[](3);
+    function onFlashLoan(bytes memory data) external {
+        require(msg.sender == address(EulerAddrsMainnet.euler), "not allowed");
 
-        operations[0] = Actions.ActionArgs({
-            actionType: Actions.ActionType.Withdraw,
-            accountId: 0,
-            amount: Types.AssetAmount({
-                sign: false,
-                denomination: Types.AssetDenomination.Wei,
-                ref: Types.AssetReference.Delta,
-                value: loanAmount
-            }),
-            primaryMarketId: 0,
-            secondaryMarketId: 0,
-            otherAddress: address(this),
-            otherAccountId: 0,
-            data: ""
-        });
+        // We still need this to protect our funds, but we can't set tx.origin in forge tests
+        /* if (tx.origin != owner) revert UnAuthOrigin(); */
 
-        operations[1] = Actions.ActionArgs({
-            actionType: Actions.ActionType.Call,
-            accountId: 0,
-            amount: Types.AssetAmount({
-                sign: false,
-                denomination: Types.AssetDenomination.Wei,
-                ref: Types.AssetReference.Delta,
-                value: 0
-            }),
-            primaryMarketId: 0,
-            secondaryMarketId: 0,
-            otherAddress: address(this),
-            otherAccountId: 0,
-            data: abi.encode(loanAmount + 2, _params)
-        });
+        ILiqqer.CallParams memory params = abi.decode(
+            data,
+            (ILiqqer.CallParams)
+        );
 
-        operations[2] = Actions.ActionArgs({
-            actionType: Actions.ActionType.Deposit,
-            accountId: 0,
-            amount: Types.AssetAmount({
-                sign: true,
-                denomination: Types.AssetDenomination.Wei,
-                ref: Types.AssetReference.Delta,
-                value: loanAmount + 2
-            }),
-            primaryMarketId: 0,
-            secondaryMarketId: 0,
-            otherAddress: address(this),
-            otherAccountId: 0,
-            data: ""
-        });
-
-        Account.Info[] memory accountInfos = new Account.Info[](1);
-        accountInfos[0] = Account.Info({owner: address(this), number: 1});
-
-        soloMargin.operate(accountInfos, operations);
-    }
-
-    function callFunction(
-        address sender,
-        Account.Info calldata accountInfo,
-        bytes calldata data
-    ) external {
-        if (msg.sender != address(soloMargin)) revert UnAuthorized();
-        if (sender != address(this)) revert UnAuthorized();
-        if (tx.origin != owner) revert UnAuthOrigin();
-
-        (uint256 loanAmountWithFee, ILiqqer.CallParams memory decoded) = abi
-            .decode(data, (uint256, ILiqqer.CallParams));
-
-        if (decoded.swaps.length > 0) {
-            for (uint256 i = 0; i < decoded.swaps.length; ) {
-                if (decoded.swaps[i].isDebtSwap == true) {
-                    IERC20 SWETH = IERC20(address(WETH));
-                    SWETH.safeIncreaseAllowance(
-                        decoded.swaps[i].allowTarget,
-                        loanAmountWithFee - 2
-                    );
-                    (bool success, ) = decoded.swaps[i].swapTarget.call(
-                        decoded.swaps[i].callData
-                    );
-                    if (!success) revert SwapCallFailed();
-                }
-                unchecked {
-                    i++;
-                }
-            }
-        }
-
-        IERC20 DTOKEN = IERC20(decoded.debtA);
-        uint256 debtB = DTOKEN.balanceOf(address(this));
-        DTOKEN.safeIncreaseAllowance(address(AAVE), debtB);
+        IERC20 DTOKEN = IERC20(params.debtA);
+        DTOKEN.safeIncreaseAllowance(address(AAVE), params.debtToCover);
 
         AAVE.liquidationCall(
-            decoded.collatA,
-            decoded.debtA,
-            decoded.userA,
+            params.collatA,
+            params.debtA,
+            params.userA,
             MAX_INT,
             false
         );
 
-        if (decoded.swaps.length > 0) {
-            for (uint256 i = 0; i < decoded.swaps.length; ) {
-                if (decoded.swaps[i].isDebtSwap == false) {
-                    IERC20 CTOKEN = IERC20(decoded.collatA);
-                    uint256 collatR = CTOKEN.balanceOf(address(this));
+        if (params.swaps.length > 0) {
+            for (uint256 i = 0; i < params.swaps.length; ) {
+                if (params.swaps[i].isDebtSwap == false) {
+                    IERC20 CTOKEN = IERC20(params.collatA);
                     CTOKEN.safeIncreaseAllowance(
-                        decoded.swaps[i].allowTarget,
-                        collatR
+                        params.swaps[i].allowTarget,
+                        params.collatToReceive
                     );
-                    (bool success, ) = decoded.swaps[i].swapTarget.call(
-                        decoded.swaps[i].callData
+                    (bool success, ) = params.swaps[i].swapTarget.call(
+                        params.swaps[i].callData
                     );
                     if (!success) revert SwapCallFailed();
                 }
@@ -155,9 +74,7 @@ contract Liqqer is Owned {
             }
         }
 
-        uint256 wethOver = WETH.balanceOf(address(this)) - loanAmountWithFee;
-        bool txsuccess = WETH.transfer(mySafe, wethOver);
-        if (!txsuccess) revert SafeTxFailed();
+        IERC20(params.debtA).transfer(msg.sender, params.debtToCover); // repay
     }
 
     function withdrawDust(address[] calldata _tokens, address _dst)
